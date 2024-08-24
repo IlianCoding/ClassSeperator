fe7cecf9732fceccf97aa1ca9f0bcef6edd9a9f9
ilianCoding <ilian.coding@gmail.com>
2024-08-24 05:28:24 +0200

    Setting up the pipeline

--- app/src/main/java/com/app/classseperation/model/configuration/layoutStrategy/GroupedLayoutStrategy.kt
+++ app/src/main/java/com/app/classseperation/model/configuration/layoutStrategy/GroupedLayoutStrategy.kt
@@ -1,4 +1,11 @@
 package com.app.classseperation.model.configuration.layoutStrategy
 
+import com.app.classseperation.model.Desk
+import com.app.classseperation.model.Student
+import com.app.classseperation.model.configuration.sortingOptions.DifferentSortingOptions
+
 class GroupedLayoutStrategy : LayoutStrategy {
+    override fun assignStudents(students: List<Student>, desks: List<Desk>, sortingOptions: DifferentSortingOptions): Boolean {
+        return false
+    }
 }
